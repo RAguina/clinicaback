d5e3bb50ca06e3416d336215f137c6498a240044
RAguina <rodrigoaguinagalde@gmail.com>
2023-11-16 01:13:12 -0300

    v0.0.91

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ app.use((req, res, next) => {
 */
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:5173/clinicaAP/', 'https://raguina.github.io', 'https://raguina.github.io/clinicaAP'];
+  const allowedOrigins = ['https://raguina.github.io/', 'https://raguina.github.io', 'https://raguina.github.io/clinicaAP'];
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
   }
 
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
