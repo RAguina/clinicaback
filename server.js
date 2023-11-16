75a9548f1056090267dd03a9d2f2be29ffa51403
RAguina <rodrigoaguinagalde@gmail.com>
2023-11-16 18:58:54 -0300

    cors a https://clinica-ap.vercel.app/news

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(cors({
 */
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://clinica-93wx832g7-rodrigos-projects-d06be1de.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://clinica-ap.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
